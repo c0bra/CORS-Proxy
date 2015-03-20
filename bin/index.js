97115155ffb90e0e7d3444a270a21ccdbee79dbf
Brian Hann <emailc0bra@gmail.com>
2015-03-20 17:44:47 -0500

    WIP

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,8 @@ var httpProxy = require("http-proxy");
 
 var host = process.env.HOST || process.argv[2] || "127.0.0.1";
 var port = process.env.PORT || process.argv[3] || 9292;
+var secure = !!process.argv[4] || false;
 
 httpProxy.createServer(corsproxy).listen(port, host, function() {
-  console.log("CORS Proxy started on %s:%d", host, port);
+  console.log("%sCORS Proxy started on %s:%d", secure && 'Secure ' || '', host, port);
 });
